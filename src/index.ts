8ccfdb72dff4e5087c72a9d733a45b6c0f263e90
“Jhonny <aniceto.0760@gmail.com>
2023-01-15 18:07:16 -0400

    Clases de desestructuracion de argumentos y importaciones y exportaciones de funciones

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,25 @@
     ===== Código de TypeScript =====
 */
 
+class Heroe {
+    alterEgo: string;
+    edad: number;
+    nombreReal: number;
 
+    /*imprimirNombre() {
+        return this.alterEgo + '  ' + this.nombreReal; //puedo hacer
+    }*/
+}
 
+/*interface Personaje2 {
+    alterEgo?: string;
+    edad?: number;
+    nombreReal?: number;
 
+    imprimirNombre:() => string; //hasta aqui se puede hacer
+}*/
 
+const iroman = new Heroe();
+//const spiderman: Personaje2 = {};
+
+console.log(iroman);
